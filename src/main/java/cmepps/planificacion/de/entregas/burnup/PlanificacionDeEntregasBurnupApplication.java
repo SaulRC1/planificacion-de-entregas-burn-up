3c26f5f3d7bd326f906239b9b7ccc17c8068d6df
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2022-12-20 09:16:39 +0100

    Empezamos proyecto

---
+++ src/main/java/cmepps/planificacion/de/entregas/burnup/PlanificacionDeEntregasBurnupApplication.java
@@ -0,0 +1,13 @@
+package cmepps.planificacion.de.entregas.burnup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanificacionDeEntregasBurnupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanificacionDeEntregasBurnupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmepps/planificacion/de/entregas/burnup/PlanificacionDeEntregasBurnupApplicationTests.java
@@ -0,0 +1,13 @@
+package cmepps.planificacion.de.entregas.burnup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanificacionDeEntregasBurnupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
