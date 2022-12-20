99ceea66e9691ef9e353f96ad5364d935b6dc6db
gonzalocasquete <gonzalocasquete2310@gmail.com>
2022-12-20 09:49:17 +0100

    Añadimos pequeña modificacion2

--- src/main/java/cmepps/planificacion/de/entregas/burnup/PlanificacionDeEntregasBurnupApplication.java
+++ src/main/java/cmepps/planificacion/de/entregas/burnup/PlanificacionDeEntregasBurnupApplication.java
@@ -9,5 +9,6 @@ public class PlanificacionDeEntregasBurnupApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlanificacionDeEntregasBurnupApplication.class, args);
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo2");
     }
 }
