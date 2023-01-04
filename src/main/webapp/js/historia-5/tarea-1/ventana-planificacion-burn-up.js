e8204749055a5462caa5904fa0df0ed5d8e01de9
gonzalocasquete <gonzalocasquete2310@gmail.com>
2023-01-04 17:36:14 +0100

    Solucionamos error de ventanas

--- src/main/webapp/js/historia-5/tarea-1/ventana-planificacion-burn-up.js
+++ src/main/webapp/js/historia-5/tarea-1/ventana-planificacion-burn-up.js
@@ -1,4 +1,4 @@
 document.getElementById("id-boton-ocultar").addEventListener("click", () => {
-    let floatinwindow=document.getElementById("float-window-background");
+    let floatinwindow=document.getElementById("float-window-background-planificacion");
     floatinwindow.style.display="none";
 });
